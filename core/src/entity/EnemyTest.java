3372c0da3b0a00c5751a4771c8dc2e4a39c88044
Marek B <DreddTop@dhcp-eduroam-hy-171-19.mobile.helsinki.fi>
2015-08-19 16:05:42 +0300

    unit tests

---
+++ core/src/entity/EnemyTest.java
@@ -0,0 +1,17 @@
+package entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by DreddTop on 19/08/15.
+ */
+public class EnemyTest {
+
+    @Test
+    public void sample() {
+
+        Assert.fail();
+    }
+
+}
--- core/src/test/EntityTest.java
+++ core/src/test/EntityTest.java
@@ -3,6 +3,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import org.junit.Test.*;
+import org.junit.Assert.AssertEquals.*;
+
 
 /**
  * Testing of the Entity class
@@ -11,6 +14,17 @@ import com.badlogic.gdx.math.Vector2;
 
 
 public class EntityTest {
+
+    @Test
+    public void sample() {
+
+        Assert.fail();
+        //System.out.println("Testing.... :D");
+        //assertEquals ('bananas', 'bananas');
+
+
+
+    }
 
 
 
